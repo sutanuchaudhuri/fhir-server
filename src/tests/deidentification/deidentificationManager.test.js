e63a3fcc11ae1f7f32021e3e0f3f27f4f9793f98
Imran Qureshi <imranq2@hotmail.com>
2023-11-27 13:43:35 -0800

    add a test for de-identification

--- src/tests/deidentification/deidentificationManager.test.js
+++ src/tests/deidentification/deidentificationManager.test.js
@@ -1,7 +1,9 @@
 const patient = require('./fixtures/patient/patient1.json');
+const condition = require('./fixtures/condition/condition1.json');
 const {describe, expect, test} = require('@jest/globals');
 const {DeidentificationManager} = require('../../deidentification/deidentificationManager');
 const Patient = require('../../fhir/classes/4_0_0/resources/patient');
+const Condition = require('../../fhir/classes/4_0_0/resources/condition');
 const fhirpath = require('fhirpath');
 // For FHIR model data (choice type support) pull in the model file:
 const fhirpath_r4_model = require('fhirpath/fhir-context/r4');
@@ -47,10 +49,25 @@ describe('deindentificationManager Tests', () => {
             console.log(result1);
             expect(result1).toStrictEqual(['PATIENT1']);
         });
-        test('test findNodeByType', async () => {
+        test('test findNodeByType for HumanName', async () => {
             const result1 = new DeidentificationManager().findNodeByType(new Patient(patient), 'HumanName');
             console.log(result1);
-            expect(result1).toStrictEqual(['PATIENT1']);
+            expect(result1.length).toBe(1);
+            expect(result1[0].given).toStrictEqual(['SHYLA']);
+        });
+        test('test findNodeByType for Reference', async () => {
+            const result1 = new DeidentificationManager().findNodeByType(new Condition(condition), 'Reference');
+            console.log(result1);
+            expect(result1.length).toBe(3);
+            expect(result1[0].reference).toStrictEqual('Patient/f001');
+            expect(result1[1].reference).toStrictEqual('Encounter/f003');
+            expect(result1[2].reference).toStrictEqual('Patient/f001');
+        });
+        test('test findNodeByType for Extension', async () => {
+            const result1 = new DeidentificationManager().findNodeByType(new Patient(patient), 'Extension');
+            console.log(result1);
+            expect(result1.length).toBe(1);
+            expect(result1[0].url).toStrictEqual('http://example.org/do-not-use/fhir-extensions/height');
         });
         test('test for nodesByType with full resource', async () => {
 
